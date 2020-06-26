e413da36d6bcc3665be5354ffb5cd2f087db035c
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-06-26 19:47:03 -0300

    removi link admin

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 </div> <!--fechando container-->
 <footer class="mt-5">
 	<div class="principal">
-		<p>Fale conosco | <a href="mailto: aprendeai@batata.com">aprendeai@batata.com</a> | <a href="?page=admin">Administre</a></p>
+		<p>Fale conosco | <a href="mailto: aprendeai@batata.com">aprendeai@batata.com</a> </p>
 		<p>© 2020 Etec | Todos os direitos reservados. Construido por Aprende Ai!.</p>
 	</div>
 </footer>
