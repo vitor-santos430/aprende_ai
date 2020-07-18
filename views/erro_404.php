d1dcd99e8edca6fa17eae0b0bead10d0bfce6269
thaiscursi <thais.cursi@gmail.com>
2020-07-18 18:48:17 -0300

    Alteração da página erro 404

--- views/erro_404.php
+++ views/erro_404.php
@@ -1,4 +1,11 @@
-<div class="container text-center my-5 py-5">
-  <h2>Erro 404</h2>
-  <span class="text-muted">Página não Encontrado</span>
+<div class="container text-center my-5 py-5" >
+  <center> 
+    <table>
+      <td><img src="./views/img/logo_batata_triste.png" height="300" width="300" style="display:inline-flex"></td>
+      <td>
+        <h2>Erro 404</h2><br>
+        <span class="text-muted">Página não Encontrado</span>
+      </td>
+    </table>
+  </center>
 </div>
